f76146176fb9754c5b14c21f3e8c051fcbb75446
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-28 15:16:07 +0530

    submitting code-Wed Aug 28 2024 15:16:04 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,54 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [inp,setInput]=useState('')
+
+  const [todo,setTodo]=useState([])
+
+  const handleBtn=()=>{
+
+    setTodo([...todo,inp])
+
+    setInput('')
+
+
+
+  }
+
+  const handleDel=(index)=>{
+
+    let filte=todo.filter((val,ind)=>ind!==index)
+
+    setTodo(filte)
+
+  }
+
+  
   return (
-    <div>
+    <div className="main">
         {/* Do not remove the main div */}
+        <div className="todo">
+          <p>T0-D0 List</p>
+          <input value={inp} onChange={(e)=>setInput(e.target.value)}/>
+          <button onClick={handleBtn}>Add Todo</button>
+        </div>
+
+        {
+      todo.map((val,index)=>(
+        <div className="dis" key={index}>
+
+          <span>{val}</span>
+          <button onClick={()=>handleDel(index)}>Delete</button>
+          
+        </div>
+      ))
+    }
     </div>
+
+   
   )
 }
 
